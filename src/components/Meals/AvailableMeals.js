159a70793f0b67d6cea33a1e89d21f37a08aec89
Paras Mittal <parmitta1@publicisgroupe.net>
2021-11-19 13:24:09 +0530

    Changes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
@@ -29,7 +30,52 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          "https://flutter-chat-8c98a-default-rtdb.firebaseio.com/meals.json"
+        );
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const responseData = await response.json();
+        const loadedMeals = [];
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+        setIsLoading(false);
+        setMeals(loadedMeals);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    };
+    fetchMeals();
+  }, []);
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  if (error) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => {
     return (
       <MealItem
         id={meal.id}
